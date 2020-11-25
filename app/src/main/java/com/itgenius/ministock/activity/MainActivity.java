fdea0e0f4a6114282fea63af7d03942dd6e8a31a
Samit Koyom <samitkoyom@gmail.com>
2020-11-25 17:32:43 +0700

    first commit

---
+++ app/src/main/java/com/itgenius/ministock/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.itgenius.ministock.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.itgenius.ministock.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
